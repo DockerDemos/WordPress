c066662bdfc98a7fb8707d42190a99a78da50769
Chris Collins <collins.christopher@gmail.com>
2014-06-18 14:16:30 -0400

    adding mod_fastcgi rpm build to support php-fpm in later builds

--- pre-install.sh
+++ pre-install.sh
@@ -8,15 +8,11 @@ name=Extra Packages for Enterprise Linux 6 - \$basearch
 mirrorlist=https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=\$basearch
 failovermethod=priority
 enabled=1
-gpgcheck=1
+gpgcheck=0
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6
 EOF
 
-/usr/bin/yum clean all
-/usr/bin/yum install -y --nogpgcheck git which pwgen cronie tar rsyslogd \
-rpm-build rpmdevtools redhat-rpm-config make gcc glibc-static
-
-# Build the Runit RPM
+# Build the RPM
 RPMUSER='rpmbuilder'
 RPMHOME="/home/$RPMUSER"
 ARCH="$(arch)"
@@ -27,10 +23,23 @@ ARCH="$(arch)"
 /bin/echo '%_topdir %(echo $HOME)/rpmbuild' > $RPMHOME/.rpmmacros
 /bin/chown -R $RPMUSER $RPMHOME
 
-/bin/su -c '/usr/bin/git clone https://github.com/imeyer/runit-rpm.git' - rpmbuilder
-/bin/su -c '/home/rpmbuilder/runit-rpm/build.sh 1>/dev/null' - rpmbuilder
+f_build() {
+	RPMSOURCE="$1"
+	RPM="$2"
+	/bin/su -c "/usr/bin/git clone $RPMSOURCE $RPM" - rpmbuilder
+	/bin/su -c "/home/rpmbuilder/$RPM/build.sh $RPM 1>/dev/null" - rpmbuilder
+}
 
-/usr/bin/yum install -y /home/rpmbuilder/rpmbuild/RPMS/$ARCH/runit-2.1.1-6.el6.$ARCH.rpm
+BUILDPKGS='rpm-build rpmdevtools redhat-rpm-config make gcc glibc-static \
+	           autoconf automake httpd httpd-devel apr-devel'
+
+/usr/bin/yum clean all
+/usr/bin/yum install -y $BUILDPKGS git which pwgen cronie tar rsyslogd
+
+f_build 'https://github.com/clcollins/mod_fastcgi-rpm.git' 'mod_fastcgi'
+f_build 'https://github.com/imeyer/runit-rpm.git' 'runit-rpm'
+
+/usr/bin/yum install -y $RPMHOME/rpmbuild/RPMS/*/*.rpm
 
 /bin/echo 'Pre-install complete'
 
